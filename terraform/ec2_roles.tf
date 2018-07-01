7f84b43831adeca83237c255106277b1a159e5bb
Dave Arnold <darnold@hashicorp.com>
2018-07-01 16:00:40 -0700

    adding ec2 roles

--- terraform/ec2_roles.tf
+++ terraform/ec2_roles.tf
@@ -3,7 +3,21 @@ resource "vault_auth_backend" "aws" {
 }
 
 resource "vault_aws_auth_backend_role" "ec2_login" {
-  backend   = "${vault_auth_backend.aws.path}"
-  role_tag  = "vault_role"
-  auth_type = "ec2"
+  backend            = "${vault_auth_backend.aws.path}"
+  bound_ami_id       = "ami-759bc50a"
+  bound_iam_role_arn = "arn:aws:iam::753646501470:instance-profile/Darnold-Hashicorp-dev"
+  role_tag           = "Role"
+  auth_type          = "ec2"
+  role               = "ec2_dev"
+  policies           = ["default", "dev"]
+}
+
+resource "vault_aws_auth_backend_role" "ec2_login" {
+  backend            = "${vault_auth_backend.aws.path}"
+  bound_ami_id       = "ami-759bc50a"
+  bound_iam_role_arn = "arn:aws:iam::753646501470:instance-profile/Darnold-Hashicorp-production"
+  role_tag           = "Role"
+  auth_type          = "ec2"
+  role               = "ec2_production"
+  policies           = ["default", "production"]
 }
