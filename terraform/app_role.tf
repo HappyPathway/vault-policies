5d75c348cda924e9b018d79636f073370f091dac
Dave Arnold <darnold@hashicorp.com>
2018-07-05 12:07:22 -0700

    adding app role metadata

--- terraform/app_role.tf
+++ terraform/app_role.tf
@@ -11,5 +11,5 @@ resource "vault_approle_auth_backend_role" "approle" {
 resource "vault_approle_auth_backend_role_secret_id" "id" {
   backend   = "${vault_auth_backend.approle.path}"
   role_name = "${vault_approle_auth_backend_role.approle.role_name}"
-  metadata  = "${file(var.approle_metadata)}"
+  metadata  = "${file("./app_roles/12factor.json")}"
 }
