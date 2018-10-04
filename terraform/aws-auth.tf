5e6436c9fc9321a96ff1aa9bc15ef9738507a6b8
David Arnold <dja+djaboxx@sonic.net>
2018-10-03 19:47:00 -0700

    Update aws-auth.tf

--- terraform/aws-auth.tf
+++ terraform/aws-auth.tf
@@ -4,7 +4,7 @@ resource "vault_auth_backend" "aws" {
 
 resource "vault_aws_auth_backend_role" "admin" {
   backend                        = "${vault_auth_backend.aws.path}"
-  role                           = "aws-iam"
+  role                           = "Darnold-Hashicorp-production"
   auth_type                      = "ec2"
   bound_account_id               = "753646501470"
   bound_iam_instance_profile_arn = "arn:aws:iam::753646501470:role/Darnold-Hashicorp-production"
