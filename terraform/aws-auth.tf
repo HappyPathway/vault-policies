a585ccba54e8a1cb763fdfccb4f118eb1fb50814
David Arnold <dja+djaboxx@sonic.net>
2018-10-03 19:39:38 -0700

    Create aws-auth.tf

---
+++ terraform/aws-auth.tf
@@ -0,0 +1,16 @@
+resource "vault_auth_backend" "aws" {
+  type = "aws"
+}
+
+resource "vault_aws_auth_backend_role" "admin" {
+  backend                        = "${vault_auth_backend.aws.path}"
+  role                           = "aws-iam"
+  auth_type                      = "ec2"
+  bound_account_id               = "753646501470"
+  bound_iam_instance_profile_arn = "arn:aws:iam::753646501470:role/Darnold-Hashicorp-production"
+  inferred_entity_type           = "ec2_instance"
+  inferred_aws_region            = "us-east-1"
+  ttl                            = 60
+  max_ttl                        = 120
+  policies                       = ["default", "dev", "production"]
+}
